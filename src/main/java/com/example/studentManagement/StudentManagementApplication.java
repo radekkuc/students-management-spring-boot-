71b3cca808756ccde4006b31bf3c201674e581d5
radekkuc <radekkuc5@gmail.com>
2025-09-09 11:44:46 +0200

    Commit from new device

--- src/main/java/com/example/studentManagement/StudentManagementApplication.java
+++ src/main/java/com/example/studentManagement/StudentManagementApplication.java
@@ -10,5 +10,7 @@ public class StudentManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
+        System.out.println("g");
 	}
+
 }
